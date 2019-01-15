7c13afb133919960794f3831f2ae361d473c743b
leoCardosoDev <leocardosodev@gmail.com>
2019-01-15 16:24:41 -0200

    Python Curso em Video 3 exercicios

---
+++ Curso-em-Video/scripts-python/desafio02.py
@@ -0,0 +1,5 @@
+dia = input('Que dia você Nasceu? (01): ')
+mes = input('Que Mês você nasceu? (Jan): ')
+ano = input('Que ano você nasce? (1900): ')
+print('Você nasceu no dia ',dia,' de ', mes, ' de ', ano, '. Correto?')
+print('Você nasceu no dia '+ dia + ' de ' + mes + ' de ' + ano + '. Correto?')
---
+++ Curso-em-Video/scripts-python/desafio03.py
@@ -0,0 +1,4 @@
+num1 = input('Digite um número: ')
+num2 = input('Digite outro numero: ')
+soma = int(num1) + int(num2)
+print('A soma é = ', soma)
